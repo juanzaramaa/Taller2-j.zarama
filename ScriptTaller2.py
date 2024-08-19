c953721cf64562922626156e36bffbda7a9d0bc6
juanzaramaa <j.zarama@uniandes.edu.co>
2024-08-19 16:19:40 -0500

    Nuevo histograma de datos generados

--- ScriptTaller2.py
+++ ScriptTaller2.py
@@ -5,3 +5,14 @@ sigma = 0.5
 n=2000
 vals = np.random.normal(loc=mu, scale=sigma, size=n)
 print(vals)
+
+# Histograma de los datos generados
+import matplotlib.pyplot as plt
+count, bins, ignored = plt.hist(x=vals, bins=30)
+plt.title('Histograma de tiempos de servicio')
+plt.xlabel('Tiempos de servicio')
+plt.ylabel('Frecuencia')
+plt.show()
+
+print(bins)
+print(count)
